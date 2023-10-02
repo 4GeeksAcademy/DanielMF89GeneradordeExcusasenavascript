d44c6064ab903371018e8fbcfc19ce550acf64f4
DanielMF89 <139999329+DanielMF89@users.noreply.github.com>
2023-10-02 19:10:54 +0000

    excusegenerator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excuse").innerHTML = excusegenerator();
 };
+let excusegenerator = () => {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whIndx = Math.floor(Math.random() * who.length);
+  let actIndex = Math.floor(Math.random() * action.length);
+  let whaIndex = Math.floor(Math.random() * what.length);
+  let wheIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whIndx] +
+    " " +
+    action[actIndex] +
+    " " +
+    what[whaIndex] +
+    " " +
+    when[wheIndex]
+  );
+};
+let refresh = document.getElementById("refresh");
+refresh.addEventListener("click", _ => {
+  location.reload();
+});
